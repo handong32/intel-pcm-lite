3c4d4f97180819ef5bab40a9476415dadf1289ce
root <root@buster.local>
2021-06-09 13:56:08 -0400

    committing simple loop add instruction tests

--- workALU.mk
+++ workALU.mk
@@ -1,6 +1,7 @@
 # make -f workALU.mk memctest COUNT=3000000
 # make -f workALU.mk memtest COUNT=3000000
 # make -f workALU.mk workALUtest COUNT=50000000
+#make -f workALU.mk sumatest COUNT=20000000
 
 .PHONEY: clean
 
@@ -13,11 +14,29 @@ memtest: jon2.o workMEM.o
 memctest: jon3.o workMEMC.o
 	g++ -g jon3.o workMEMC.o Msr.o Cpuid.o Perf.o Rapl.o -o $@
 
+sumtest: jon4.o workSUM.o
+	g++ -g jon4.o workSUM.o Msr.o Cpuid.o Perf.o Rapl.o -o $@
+
+sumatest: jon5.o workSUMA.o
+	g++ -g jon5.o workSUMA.o Msr.o Cpuid.o Perf.o Rapl.o -o $@
+
 jon2: jon2.cc
 	g++ -g -c  $< -o $@
 
 jon3: jon3.cc
 	g++ -g -c  $< -o $@
+
+jon4: jon4.cc
+	g++ -g -c  $< -o $@
+
+jon5: jon5.cc
+	g++ -g -c  $< -o $@
+
+workSUM: workSUM.c
+	g++ -c -DREPEAT_COUNT=$(COUNT) -Wa,-adhln -g $< -o $@
+
+workSUMA.o: workSUMA.S
+	g++ -c -DREPEAT_COUNT=$(COUNT) -Wa,-adhln -g $< -o $@
 
 main.o: jon1.cc
 	g++ -g -c  $< -o $@
@@ -32,4 +51,4 @@ workMEMC.o: workMEMC.S
 	g++ -c -DREPEAT_COUNT=$(COUNT) -Wa,-adhln -g $< -o $@
 
 clean:
-	-${RM} workALU.o main.o jon2.o workMEM.o workALUtest memtest memctest workMEMC.o jon3.o
+	-${RM} workALU.o main.o jon2.o workMEM.o workALUtest memtest memctest workMEMC.o jon3.o jon4.o workSUM.o workSUMA.o jon5.o sumtest sumatest
---
+++ workSUM.c
@@ -0,0 +1,7 @@
+void sum(unsigned long long *a, unsigned long long slen)
+{
+  unsigned long long i;
+  for(i=0;i<slen;i++) {
+    *a += 1;
+  }
+}
